a9eaca74f62f27a48497ef0ea94233e663731efd
Cody Snyder <99768792+cwsnyder@users.noreply.github.com>
2022-06-15 16:11:37 -0400

    Update Comments.jsx

--- React/Comments.jsx
+++ React/Comments.jsx
@@ -1,45 +1,16 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import CommentCard from './CommentCard';
 import AddEditComment from './AddEditComment';
 import debug from 'sabio-debug';
 import PropTypes from 'prop-types';
 import commentService from '../../services/commentService';
-import avatar3 from '../../assets/images/users/avatar-3.jpg';
-import avatar4 from '../../assets/images/users/avatar-4.jpg';
 
 const _logger = debug.extend('Comments');
 
-function Comment(props) {
-    const [user] = useState({ id: 1, avatar: avatar3 });
-    const [postData, setPostData] = useState({
-        id: 2,
-        author: {
-            id: 1,
-            name: 'Thelma Fridley',
-            avatar: avatar4,
-        },
-        postedOn: 'about 1 hour ago',
-        content:
-            '<div class="font-16 text-center fst-italic text-dark"><i class="mdi mdi-format-quote-open font-20"></i> Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Duis sagittis ipsum. Praesent mauris. Fusce nec tellus sed augue semper porta. Mauris massa.</div>',
-        totalLikes: '28',
-        totalComments: '',
-        isLiked: true,
+function Comments(props) {
+    const [entityData, setEntityData] = useState({
         isToggled: false,
         isReplying: false,
-        comments: [
-            {
-                id: 1,
-                content: 'Nice work, makes me think of The Money Pit.',
-                postedOn: '3 hours ago',
-                author: {
-                    id: 2,
-                    name: 'Jeremy Tomlinson',
-                    avatar: avatar3,
-                },
-                isLiked: true,
-            },
-        ],
-        engagement: true,
     });
 
     const [commentData, setCommentData] = useState({
@@ -47,19 +18,14 @@ function Comment(props) {
         commentComponents: [],
     });
 
-    const reply = commentData.arrayOfComments;
+    const entityType = props.entityTypeId;
+    const entityId = props.entityId;
 
-    _logger('comment form state', props);
-
-    _logger('comment reply', reply);
-
-    useEffect(() => {
-        renderComments();
-    }, []);
-
-    const renderComments = () => {
-        commentService.getCommentByEntityId(1, 11).then(onGetSuccess).catch(onGetError);
+    const getComments = () => {
+        commentService.getCommentByEntityId(entityType, entityId).then(onGetSuccess).catch(onGetError);
     };
+
+    _logger('comments props', props);
 
     const onGetSuccess = (response) => {
         _logger('Get Success response', response);
@@ -76,32 +42,32 @@ function Comment(props) {
         _logger('GetCreatedBy Error', error);
     };
 
-    const createMarkup = (text) => {
-        return { __html: text };
-    };
-
     const mapComment = (aComment) => {
-        _logger('aComment', aComment);
+        _logger('commentMap', aComment);
         return (
             <CommentCard
                 key={aComment.id}
                 comment={aComment}
-                refresh={renderComments}
+                refresh={getComments}
+                entityTypeId={props.entityTypeId}
+                entityId={props.entityId}
                 currentUser={props.currentUser}></CommentCard>
         );
     };
 
     const onIsToggled = () => {
-        setPostData((prevState) => {
+        setEntityData((prevState) => {
             const sp = { ...prevState };
             sp.isToggled = !prevState.isToggled;
             return sp;
         });
+        if (entityData.isToggled !== true) {
+            getComments();
+        }
     };
 
-    const onIsReplying = (e) => {
-        _logger('reply button hit', e);
-        setPostData((prevState) => {
+    const onIsReplying = () => {
+        setEntityData((prevState) => {
             const sp = { ...prevState };
             sp.isReplying = !prevState.isReplying;
             return sp;
@@ -110,63 +76,59 @@ function Comment(props) {
 
     return (
         <React.Fragment>
-            <div className="border border-light rounded p-2 mb-3">
-                <div className="d-flex">
-                    <img className="me-2 rounded-circle" src={postData.author.avatar} alt="" height="32" />
-                    <div>
-                        <h5 className="m-0">{postData.author.name}</h5>
-                        <p className="text-muted">
-                            <small>{postData.postedOn}</small>
-                        </p>
+            <div
+                className="body-container mx-auto mb-5"
+                style={{ width: 800, borderStyle: 'solid', borderWidth: 0.25 }}>
+                <h3 className="text-center mb-3 mt-3" style={{ color: 'green' }}>
+                    Comments
+                </h3>
+                {
+                    <div className="mx-n2 p-2">
+                        {
+                            <AddEditComment
+                                refresh={getComments}
+                                entityTypeId={props.entityTypeId}
+                                entityId={props.entityId}
+                                replyButton={onIsReplying}
+                                currentUser={props.currentUser}
+                                parentId={0}></AddEditComment>
+                        }
                     </div>
-                </div>
+                }
 
-                <div dangerouslySetInnerHTML={createMarkup(postData.content)}></div>
                 <div className="row">
-                    <div className="col-2 mt-2">
-                        {!postData.content ? null : (
-                            <button className="btn btn-sm btn-link text-muted" onClick={onIsReplying}>
-                                <i className="reply"></i> Reply
+                    <div className="text-center mt-2 mb-2">
+                        {!entityData.isToggled ? (
+                            <button className="btn btn-lg" style={{ color: 'brown' }} onClick={onIsToggled}>
+                                View All Comments
+                            </button>
+                        ) : (
+                            <button className="btn btn-lg" onClick={onIsToggled} style={{ color: 'brown' }}>
+                                Close Comments
                             </button>
                         )}
                     </div>
-                    <div className="col-2 mt-2">
-                        {postData.comments ? (
-                            <button className="btn btn-sm btn-link text-muted" onClick={onIsToggled}>
-                                <i className="reply"></i> View Comments
-                            </button>
-                        ) : null}
+                </div>
+
+                {entityData.isToggled ? (
+                    <div className="comment-container">
+                        <div className="mx-n2 me-2 p-2 mt-3">{commentData.commentComponents}</div>
                     </div>
-                </div>
+                ) : null}
             </div>
-
-            {postData.isReplying ? (
-                <div className="mx-n2 p-2 bg-light">
-                    {user && (
-                        <AddEditComment
-                            refresh={renderComments}
-                            replyButton={onIsReplying}
-                            parentId={0}></AddEditComment>
-                    )}
-                </div>
-            ) : null}
-
-            {postData.isToggled ? (
-                <div className="comment-container">
-                    <div className="mx-n2 p-2 mt-3 bg-light">{commentData.commentComponents}</div>
-                </div>
-            ) : null}
         </React.Fragment>
     );
 }
 
-Comment.propTypes = {
+Comments.propTypes = {
     currentUser: PropTypes.shape({
         id: PropTypes.number,
         roles: PropTypes.arrayOf(PropTypes.string),
         email: PropTypes.string,
         isLoggedIn: PropTypes.bool,
     }),
+    entityTypeId: PropTypes.number.isRequired,
+    entityId: PropTypes.number.isRequired,
 };
 
-export default Comment;
+export default Comments;

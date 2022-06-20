bf0e1eda2d687f87e6417a096912c5e3cb670560
Cody Snyder <99768792+cwsnyder@users.noreply.github.com>
2022-06-20 16:06:45 -0400

    Update CommentSelect.sql

--- SQL/CommentSelect.sql
+++ SQL/CommentSelect.sql
@@ -1,13 +1,15 @@
-ALTER  proc [dbo].[Comments_Select_ByCreatedBy]
-				@CreatedBy int
-
+ALTER proc [dbo].[Comments_Select_ByEntityId]
+					@EntityTypeId int
+					,@EntityId int
 AS
 
 /*
-	DECLARE @Id int = 2
+	DECLARE @EntityTypeId int = 1
+			,@EntityId int = 11
 
-	EXECUTE [dbo].[Comments_Select_ByCreatedBy]
-				@Id
+	EXECUTE [dbo].[Comments_Select_ByEntityId]
+					@EntityTypeId
+					,@EntityId
 
 */
 
@@ -23,15 +25,43 @@ BEGIN
 		,c.[DateModified]
 		,c.[CreatedBy]
 		,c.[IsDeleted]
-		,Replies = ( Select *
-					FROM [dbo].Comments
-					WHERE (ParentId = c.Id 
-					AND EntityId = c.EntityId)
-					FOR JSON AUTO)
+		,up.[FirstName]
+		,up.[LastName]
+		,up.[AvatarUrl]
+		,Replies = 
+			( 
+			SELECT
+				ct.[Id]
+				,ct.[Subject]
+				,ct.[Text]
+				,ct.[ParentId]
+				,ct.[EntityTypeId]
+				,ct.[EntityId]
+				,ct.[DateCreated]
+				,ct.[DateModified]
+				,ct.[CreatedBy]
+				,ct.[IsDeleted]
+				,ups.[FirstName]
+				,ups.[LastName]
+				,ups.[AvatarUrl]
+			FROM [dbo].Comments as ct 
+			INNER JOIN [dbo].[UserProfiles] as ups
+				on ct.CreatedBy = ups.UserId
+										
+			WHERE (	ct.ParentId = c.Id 
+				AND ct.[EntityId] = c.EntityId
+				AND ct.[IsDeleted] = 0)
+			FOR JSON PATH
+			)
 
-	FROM [dbo].[Comments] as c INNER JOIN [dbo].[Users] as u
-						on c.[CreatedBy] = u.[Id]
-	WHERE (c.[CreatedBy] = @CreatedBy
+	FROM [dbo].[Comments] as c INNER JOIN [dbo].EntityTypes as e
+							on c.EntityTypeId = e.Id
+							INNER JOIN [dbo].Users as u
+							on c.CreatedBy = u.Id
+							INNER JOIN [dbo].[UserProfiles] as up
+							on c.CreatedBy = up.UserId
+	WHERE (c.[EntityId] = @EntityId
+			AND c.[EntityTypeId] = @EntityTypeId
 			AND c.[ParentId] = 0
 			AND c.[IsDeleted] = 0)
 
